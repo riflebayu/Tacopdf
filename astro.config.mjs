ce68209d6d3e459d943132d7c4ca30892d2cbbbe
riflebayu <riflebayu@gmail.com>
2026-08-03 18:21:30 +0700

    asa

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [react()],
   vite: {
